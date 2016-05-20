9743f2ab02a7b65f2d4cf8d50af3156be4900346
tpdo33 <tpdo33@gmail.com>
2016-05-19 23:52:14 -0700

    first commit.... created listings function

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,52 @@
+class ListingsController < ApplicationController
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+
+  # GET /listings
+  def index
+    @listings = Listing.all
+  end
+
+  def show
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def edit
+  end
+
+  # create new listing
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      flash[:success] = "Listing has successfully been created"
+      redirect_to listing_path(@listing)
+   else
+    flash[:errors] = @listing.errors.full_messages
+    redirect_to '/listings'
+  end
+  end
+
+  def update
+    @listing.update(listing_params)
+    redirect_to listing_path(@listing) 
+  end
+
+  def destroy
+    @listing.destroy
+    respond_to do |format|
+      format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def listing_params
+      params.require(:listing).permit(:name, :description, :price)
+    end
+    def set_listing
+      @listing = Listing.find(params[:id])
+    end
+
+end
