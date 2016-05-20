96c91e332fc7a629c63b77d6d3167b9e9a5ab363
tpdo33 <tpdo33@gmail.com>
2016-05-20 14:21:28 -0700

    production env fix attempt #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Required for Devise... remember to change localhost:3000 to actual app host
-  config.action_mailer.default_url_options = { host: 'http://estyclone-tdo.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'http://estyclone-tdo.heroku.com/'}
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
